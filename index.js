4645898f8b167bb350e6f48de01405d1fb1d9ec8
Dastin Kuwalek <d.kuwalek@gmail.com>
2018-11-18 11:53:40 +0100

    First part
    some JavaScript

---
+++ index.js
@@ -0,0 +1,46 @@
+let imagineros = ['./imaginaros/IMG_3271.JPG','./imaginaros/IMG_3207.JPG','./imaginaros/IMG_3288.JPG'];
+let activeIndex = 0;
+let time = 1000;
+let imageirnoSliderinoElementerino = document.querySelector(".imagerino-sliderino");
+
+function changerinoSliderino(){
+    if (this.className === 'prawerinio') {
+        activeIndex+=1;
+    } else {
+        activeIndex-=1;
+    }
+    if (activeIndex < 0) activeIndex = imagineros.length-1;
+    else if (activeIndex == imagineros.length) activeIndex =0;
+    console.log("Activindex: " + activeIndex);
+    imageirnoSliderinoElementerino.src = imagineros[activeIndex];
+}
+changerinoSliderino();
+
+let buttonesChangerones = document.getElementsByTagName("button");
+for (let i = 0; i <buttonesChangerones.length; i++) {
+    buttonesChangerones[i].addEventListener("click", changerinoSliderino);
+}
+// function switcherinoToNexterino () {
+//     imageirnoSliderinoElementerino.src = imagineros[activeIndex];
+//     if(activeIndex == (imagineros.length-1)){
+//         activeIndex=0
+
+//     } else {
+//         activeIndex++;
+//     }
+//     console.log(imageirnoSliderinoElementerino);
+//     console.log("Current activeIndex value " + activeIndex);
+//     setTimeout(switcherinoToNexterino, time);
+// }
+// switcherinoToNexterino();
+
+// function showerinoImagerino() {
+//     let i = 0, length = imagineros.length;
+//     for (;i < length; i++) {
+//         let img = new Image();
+//         img.src = imagineros[i];
+//         img.style.width = '400px';
+//         document.getElementById("imagineros-listeros").appendChild(img);
+//     }
+// }
+// showerinoImagerino();
